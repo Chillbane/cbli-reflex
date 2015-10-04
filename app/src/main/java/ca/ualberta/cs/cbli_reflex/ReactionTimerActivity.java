b75271801eea7fefa4e739a411b227d24fc48647
cbli <cbli@ualberta.ca>
2015-10-04 04:25:49 -0600

    Finished reaction timer.

--- app/src/main/java/ca/ualberta/cs/cbli_reflex/ReactionTimerActivity.java
+++ app/src/main/java/ca/ualberta/cs/cbli_reflex/ReactionTimerActivity.java
@@ -1,8 +1,11 @@
 package ca.ualberta.cs.cbli_reflex;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.DialogFragment;
 import android.content.DialogInterface;
+import android.os.Handler;
+import android.os.Looper;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Gravity;
@@ -11,8 +14,13 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class ReactionTimerActivity extends ActionBarActivity {
+    protected long startTime;
+    protected long endTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,15 +28,6 @@ public class ReactionTimerActivity extends ActionBarActivity {
         setContentView(R.layout.reaction_timer_wait);
 
         displayMessage("When you see GO, tap as fast as you can");
-
-        Button waitButton = (Button) findViewById(R.id.reactionTimerWaitButton);
-        waitButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-
-            }
-        });
-
-        waitButton.setText("GO");
     }
 
     @Override
@@ -53,19 +52,101 @@ public class ReactionTimerActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /* Retrieved from jeevamuthu, http://stackoverflow.com/questions/3965122/android-how-to-
+    /* Modified from jeevamuthu, http://stackoverflow.com/questions/3965122/android-how-to-
      * align-message-in-alertdialog, 10/02/15
      */
     // Display prompt with message
     public void displayMessage(String text) {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setMessage(text);
-        builder.setPositiveButton("OK", null);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int BUTTON_POSITIVE) {
+                beginTest();
+            }
+        });
         AlertDialog dialog = builder.show();
         TextView messageText = (TextView)dialog.findViewById(android.R.id.message);
         messageText.setGravity(Gravity.CENTER);
         dialog.show();
     }
 
+    /* Retrived from pomber, http://stackoverflow.com/questions/3072173/how-to-call-a-method-
+     * after-a-delay-in-android, 10/03/15
+     */
+    public void beginTest(){
+
+        int waitTime = generateRandomWaitTime();
+
+        // Detect early click
+        Button waitButton = (Button) findViewById(R.id.reactionTimerWaitButton);
+        waitButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                v.setEnabled(false);
+
+                Toast.makeText(getApplicationContext(), "Don't tap until you see GO",
+                        Toast.LENGTH_SHORT).show();
+
+                restartActivity();
+            }
+        });
+
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                recordReactionTime();
+
+            }
+        }, waitTime);
+    }
+
+    /* Retrieved from Vivien Barousse, http://stackoverflow.com/questions/6029495/how-can-i-
+       generate-random-number-in-specific-range-in-android, 10/03/15
+     */
+
+    public int generateRandomWaitTime() {
+        int min = 10;
+        int max = 2000;
+
+        Random r = new Random();
+
+        return r.nextInt(max - min + 1) + min;
+    }
+
+    public void recordReactionTime(){
+        startTime = (System.nanoTime() / Math.round(Math.pow(10, 6)));
+
+        Button waitButton = (Button) findViewById(R.id.reactionTimerWaitButton);
+        waitButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                v.setEnabled(false);
+                endTime = (System.nanoTime() / Math.round(Math.pow(10, 6)));
+                long reactionTime = endTime - startTime;
+
+                ReactionTimerController rtc = new ReactionTimerController();
+                rtc.addReactionTime(reactionTime);
+
+                /* Modified from Pushp Raj Saurabh, http://stackoverflow.com/questions/17716813/
+                cannot-resolve-maketext-method-of-toast, 10/04/15
+                 */
+                Toast.makeText(getApplicationContext(), "Reaction time: " + reactionTime + " ms",
+                        Toast.LENGTH_SHORT).show();
+
+                restartActivity();
+            }
+        });
+
+        waitButton.setText("GO");
+    }
+
+    public void restartActivity() {
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                recreate();
+            }
+        }, 1000);
+    }
 
 }
---
+++ app/src/main/java/ca/ualberta/cs/cbli_reflex/ReactionTimerController.java
@@ -0,0 +1,42 @@
+/*
+Student Picker: Randomly pick students to answer questions
+
+Copyright (C) 2015 Carin Li Abram Hindle abram.hindle@softwareprocess.ca
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package ca.ualberta.cs.cbli_reflex;
+
+/*
+ * Created by Carin on 10/2/2015.
+ */
+public class ReactionTimerController {
+
+    // Lazy singleton
+    private static ReactionTimeList reactionTimeList = null;
+    static public ReactionTimeList getReactionTimeList() {
+        if (reactionTimeList == null) {
+            reactionTimeList = new ReactionTimeList();
+        }
+        return reactionTimeList;
+    }
+
+    public void addReactionTime(long reactionTime) {
+        getReactionTimeList().addReactionTime(reactionTime);
+    }
+
+    public long getReactionTime(int i) {
+        return getReactionTimeList().getReactionTime(i);
+    }
+
+}
